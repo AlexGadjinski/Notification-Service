198bdd23121287d6d08887e698dc70edd57bc378
Aleksandar Gadzhinski <alexgadjinski@gmail.com>
2026-02-05 12:50:52 +0200

    removed unnecessary annotations for dto classes

--- src/main/java/app/web/dto/UpsertNotificationPreference.java
+++ src/main/java/app/web/dto/UpsertNotificationPreference.java
@@ -2,11 +2,11 @@ package app.web.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
+import lombok.Getter;
 
 import java.util.UUID;
 
-@Data
+@Getter
 public class UpsertNotificationPreference {
 
     @NotNull
